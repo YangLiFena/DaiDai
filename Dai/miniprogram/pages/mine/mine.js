8207598f1d74962a23ecacc5f097bdda283dd761
blacksheep107 <blacksheep107@126.com>
2020-11-13 11:30:17 +0800

    认证信息加入数据库，新增跳转

---
+++ Dai/miniprogram/pages/mine/mine.js
@@ -0,0 +1,46 @@
+Page({
+  data: {
+    catelist:[
+      {
+        id:1,
+        image_src: '/photo/w1.png'
+      },
+      {
+        id:2,
+        image_src: '/photo/w2.png'
+      },
+      {
+        id:3,
+        image_src: '/photo/w3.png'
+      }
+    ]
+  },
+  onLoad: function (options)
+{
+  
+  var that = this;
+  // 查看是否授权
+  wx.getSetting({
+  success(res) {
+  if (res.authSetting['scope.userInfo']) {
+  // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+  wx.getUserInfo({
+  success: function (res) {
+  console.log(res)
+  // console.log(res.userInfo)
+  that.setData({
+  nickName: res.userInfo.nickName, //昵称
+  avatarUrl: res.userInfo.avatarUrl //头像
+  })
+  }
+  })
+  }else{
+  wx.redirectTo({
+  url: '/pages/login_/login_', //跳转到授权页面
+  })
+  }
+  }
+  })
+  }
+})
+
